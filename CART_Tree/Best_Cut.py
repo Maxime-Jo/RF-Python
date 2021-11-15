5d6fd7406c4630c3a75cea522b6c15805eb886f8
Maxime Jo <neogardml@gmail.com>
2021-11-15 17:06:31 -0500

    Solve issue for prediction

--- CART_Tree/Best_Cut.py
+++ CART_Tree/Best_Cut.py
@@ -51,7 +51,7 @@ class Best_cut:
         
         root_purity = MoD.MeasureOfDispersion(y,[])
         
-        splits_evaluation = np.array([[len(y),np.nan,root_purity, -1]])    # ensure that previous split is not better
+        self.splits_evaluation = np.array([[len(y),np.nan,root_purity, -1]])    # ensure that previous split is not better
         
         for f in range(0,X.shape[1]):
             
@@ -65,15 +65,36 @@ class Best_cut:
             
             cut = len(x_left)
             
-            splits_evaluation = np.concatenate((splits_evaluation, [[cut, cut_value, purity, f]]),0)
+            self.splits_evaluation = np.concatenate((self.splits_evaluation, [[cut, cut_value, purity, f]]),0)
             
+        # BEST PURITY  
+        best_purity = np.min(self.splits_evaluation[:,2])
+        binary_filter_puriy = self.splits_evaluation[:,2] == best_purity
+        # BEST CUT ON PURITY
+        """ it may exist multiple best purity"""
+        f_tmp = self.splits_evaluation[binary_filter_puriy,3].min()   
+        binary_filter_feature = self.splits_evaluation[:,3] == f_tmp
+        # FINAL SELECTED CUT
+        binary_filter = np.logical_and(binary_filter_puriy, binary_filter_feature)
         
-        best_purity = np.min(splits_evaluation[:,2])
-        cut_value = splits_evaluation[splits_evaluation[:,2] == best_purity,1][0]
-        cut = splits_evaluation[splits_evaluation[:,2] == best_purity,0][0]
-        feature = splits_evaluation[splits_evaluation[:,0] == cut, 3][0]
+        if binary_filter.sum() > 1:
+            print("ATTENTION MULTIPLE BEST PURITY")
+            print(self.splits_evaluation)
+        elif binary_filter.sum() ==0:
+            print("ATTENTION NO BEST PURITY")
+               
+        cut_value = self.splits_evaluation[binary_filter,1][0]
+        feature = self.splits_evaluation[binary_filter, 3][0]
+        
+        record = X[:,int(feature)]<=cut_value
+        record = abs(record.astype(int)-1)
+        
+        if feature == -1:
+            cut_type = "root"
+        else:
+            cut_type = "ok"
 
-        return cut, feature, cut_value
+        return cut, feature, cut_value, cut_type, record
             
 
 
@@ -81,16 +102,22 @@ class Best_cut:
 test
 """
 
-#BC = Best_cut()
-#cut, feature, cut_value = BC.visit_all_features(X_train,y_train)
+# BC = Best_cut()
 
 
 
+# cut, feature, cut_value, cut_type, record = BC.visit_all_features(X,y)
 
 
 
+# X_1 = X[X[:,5]<=6.939,:]
+# y_1 = y[X[:,5]<=6.939]
+# cut, feature, cut_value, cut_type, record = BC.visit_all_features(X_1,y_1)
 
 
+# X_2 = X_1[X_1[:,12]<=14.37,:]
+# y_2 = y_1[X_1[:,12]<=14.37]
+# cut, feature, cut_value, cut_type, record = BC.visit_all_features(X_2,y_2)
 
 
 
