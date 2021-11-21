d27affbea01aa0df3572d38c7d65201814c90a61
Maxime-Jo <78235958+Maxime-Jo@users.noreply.github.com>
2021-11-21 11:40:06 -0500

    Update Prediction.py

--- CART_Tree/Prediction.py
+++ CART_Tree/Prediction.py
@@ -69,14 +69,12 @@ class Prediction:
             y_records[boolean_child_1, k] = child_1
             y_records[boolean_child_2, k] = child_2
    
-        y_test_pred = np.zeros((len(X))) 
+        y_test_pred = np.zeros((len(X))) + float('inf')
+        y_records_last = y_records[:,-1]
         
-        #Simplify function ()
-        for m in range(0, len(train_terminal_pred)):
-            for j in range(0,len(y_records)):
-                if y_records[j,-1] == train_terminal_pred[m, 1]:
-                    y_test_pred[j] = train_terminal_pred[m,0]
-
+        for pred in train_terminal_pred:
+            y_test_pred[y_records_last==pred[1]] = pred[0]
+ 
         return (y_test_pred)
                     
     def Loop_Pred(self, X, L_records, L_root_tree_building, L_train_pred):
@@ -90,42 +88,7 @@ class Prediction:
             y_train_pred = L_train_pred[v]
             
             y_test_pred = self.Test_Pred(X, y_records_train, root_tree_building_train, y_train_pred)
-                               
-                    # train_terminal_pred = np.column_stack([y_train_pred, y_records_train[:,-1]])
-                    # train_terminal_pred = np.unique(train_terminal_pred, axis = 0)
-                    
-                    # y_records = np.zeros((len(X),len(root_tree_building_train)))
-                    # y_records[:,0] = np.random.randint(0,1,len(X)) + 1  
-      
-                    
-                    # for k in range(1, len(root_tree_building_train)):
-                    
-                    #     node_cut = root_tree_building_train[k,0]
-                    #     cut_value = root_tree_building_train[k,1]
-                    #     feature = root_tree_building_train[k,2]
-                    #     feature = feature.astype(int)
-                        
-                    #     child_1 = node_cut*2
-                    #     child_2 = child_1 + 1
-                    #     m = k-1
-                        
-                    #     boolean_child_1 = np.logical_and(X[:, feature] <= cut_value,
-                    #                              y_records[:,m]==node_cut)
-                    #     boolean_child_2 = np.logical_and(X[:, feature] > cut_value,
-                    #                              y_records[:,m]==node_cut)
-                        
-                    #     y_records[:,k] =  y_records[:,m]
-                    #     y_records[boolean_child_1, k] = child_1
-                    #     y_records[boolean_child_2, k] = child_2
-               
-                    # y_test_pred = np.zeros((len(X))) 
-                    
-                    # #Simplify function ()
-                    # for m in range(0, len(train_terminal_pred)):
-                    #     for j in range(0,len(y_records)):
-                    #         if y_records[j,-1] == train_terminal_pred[m, 1]:
-                    #             y_test_pred[j] = train_terminal_pred[m,0]
-                    
+   
             y_pred.append(y_test_pred)
                     
         y_pred = np.transpose(np.vstack(y_pred))
